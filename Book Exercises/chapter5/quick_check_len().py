24e7cc79c2d9262a8c25b71e23748961bff76d78
AminRezaei <4min.Rezaei@gmail.com>
2022-05-08 11:59:32 +0430

    chapter5

---
+++ Book Exercises/chapter5/quick_check_len().py
@@ -0,0 +1,8 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+len([0])
+1
+len([])
+0
+len([[1, 3, [4, 5], 6], 7])
+2

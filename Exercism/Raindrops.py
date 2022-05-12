760b638517110d4a52a017e089dce52b71b59d6b
AminRezaei <4min.Rezaei@gmail.com>
2022-05-12 19:20:49 +0430

    Raindrops version2.0

--- Exercism/Raindrops.py
+++ Exercism/Raindrops.py
@@ -1,13 +1,8 @@
 def convert(number):
-    sound_should_be=[]
     number = int(number)
-    if number % 3 == 0:
-        sound_should_be.append('Pling')
-    if number % 5 == 0:
-        sound_should_be.append('Plang')
-    if number % 7 == 0:
-        sound_should_be.append('Plong')
-    if len(sound_should_be )>0:
-        return(''.join(sound_should_be))
+    dict = {3:'Pling', 5:'Plang', 7:'Plong'}
+    ls = [dict[i] for i in dict.keys() if number % i == 0]
+    if len(ls) > 0:
+        return ''.join(ls)
     else:
-        return(str(number))
+        return str(number)

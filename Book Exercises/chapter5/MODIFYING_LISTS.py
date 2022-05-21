31f4c0421f0d46cccb7b45ca7f21d2295df1cd11
AminRezaei <4min.Rezaei@gmail.com>
2022-05-21 14:14:54 +0430

    chapter5:  MODIFYING LISTS

---
+++ Book Exercises/chapter5/MODIFYING_LISTS.py
@@ -0,0 +1,9 @@
+'''
+Suppose that you have a list 10 items long. How
+might you move the last three items from the end of the list to the beginning,
+keeping them in the same order?
+'''
+ls = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = ls[-3:]
+del ls [-3:]
+ls[:0] = a

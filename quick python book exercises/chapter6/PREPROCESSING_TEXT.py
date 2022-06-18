28674f9b72ccbeddda73c70953234d92bab8926b
AminRezaei <4min.Rezaei@gmail.com>
2022-06-18 18:12:09 +0430

    chapter 6 is done

--- quick python book exercises/chapter6/PREPROCESSING_TEXT.py
+++ quick python book exercises/chapter6/PREPROCESSING_TEXT.py
@@ -4,7 +4,6 @@ In this lab,
 the task is to read the first part of the first chapter of Moby Dick (found in the book's source code),
 make sure that everything is one case, remove all punctuation, and write the words one per line to a second file.
 '''
-import re
 import string
 
 infile = open("moby_01.txt")
